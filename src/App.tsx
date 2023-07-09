6c4cbc5fad9f5dfe6cebe549570f89b609c219cf
ashgray <7ashgray7@gmail.com>
2023-07-09 13:34:04 +0900

    muiの導入/タブでコンテンツの出しわけ

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, ReactNode } from "react";
+
+import "./App.css";
+
+import { Box } from "@mui/material";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+
+function TabContents(props: {
+  children?: ReactNode;
+  chosenTabIndex: number;
+  contentsTabIndex: number;
+}) {
+  return (
+    <Box>
+      {props.chosenTabIndex === props.contentsTabIndex && (
+        <Box>{props.children}</Box>
+      )}
+    </Box>
+  );
+}
 
 function App() {
+  const [chosenTabIndex, choiseTab] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <Tabs value={chosenTabIndex} centered>
+          <Tab onClick={() => choiseTab(0)} label="ALL"></Tab>
+          <Tab onClick={() => choiseTab(1)} label="NOT YET"></Tab>
+          <Tab onClick={() => choiseTab(2)} label="DOING"></Tab>
+          <Tab onClick={() => choiseTab(3)} label="COMPLETE"></Tab>
+        </Tabs>
+      </Box>
+      <TabContents chosenTabIndex={chosenTabIndex} contentsTabIndex={0}>
+        全てのタスク
+      </TabContents>
+      <TabContents chosenTabIndex={chosenTabIndex} contentsTabIndex={1}>
+        未着手のタスク
+      </TabContents>
+      <TabContents chosenTabIndex={chosenTabIndex} contentsTabIndex={2}>
+        実行中のタスク
+      </TabContents>
+      <TabContents chosenTabIndex={chosenTabIndex} contentsTabIndex={3}>
+        完了ののタスク
+      </TabContents>
     </div>
   );
 }
